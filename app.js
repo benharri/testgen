de00ee1c17a3fe766b145674ecf2514f9db3e2e2
Ben Harris <bharris@hagerty.com>
2017-09-08 14:54:02 -0400

    formatting

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 function cartesian(arr) {
     return arr.reduce(function (a, b) {
-    return a.map(function (x) {
-        return b.map(function (y) {
-            return x.concat(y);
-        })
-    }).reduce(function (a, b) { return a.concat(b) }, [])
-    }, [[]])
+        return a.map(function (x) {
+            return b.map(function (y) {
+                return x.concat(y)
+            })
+        }).reduce(function (a, b) {
+            return a.concat(b)
+        }, [])
+    }, [
+        []
+    ])
 }
 
 const vm = new Vue({
