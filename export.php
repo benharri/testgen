551047edb67717d5e03f719dbff2c8699b7126be
Ben Harris <bharris@hagerty.com>
2017-10-25 15:50:27 -0400

    add check for empty post

--- export.php
+++ export.php
@@ -2,15 +2,18 @@
 
 require_once __DIR__ . "/vendor/autoload.php";
 
+if (empty($_POST["test_cases"])) {
+    header("Location: /~ben/testgen/");
+    die();
+}
 
-$excel = PHPExcel_IOFactory::load("export.xlsx");
-
+// split input by line and place into template at cell B2
 $cases = explode("\n", $_POST["test_cases"]);
+$excel = PHPExcel_IOFactory::load("template.xlsx");
 $excel->getActiveSheet()->fromArray(array_chunk($cases, 1), NULL, 'B2');
 
+// set the right download headers
 header('Content-Type: application/vnd.ms-excel');
 header('Content-Disposition: attachment;filename="testcase-template.xls"');
 header('Cache-Control: max-age=0');
-$writer = PHPExcel_IOFactory::createWriter($excel, 'Excel5');
-$writer->save('php://output');
-
+PHPExcel_IOFactory::createWriter($excel, 'Excel5')->save('php://output');
